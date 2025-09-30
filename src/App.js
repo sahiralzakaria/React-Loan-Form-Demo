8f910134088891bb423fa9312e38fc253611d202
sahir zakaria <sahir.alzakaria@gmail.com>
2025-09-30 07:44:02 +0300

    finish app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 
 import LoanForm from "./LoanForm";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
   return (
-    <div className="App" style={{ marginTop: "5%" }}>
-      <LoanForm />
-    </div>
+    <UserContext.Provider
+      value={{
+        userName: "aliii",
+        name: "aliii addd",
+        email: "saas@gmmma.com",
+      }}
+    >
+      <div className="App" style={{ marginTop: "5%" }}>
+        <LoanForm />
+      </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export let UserContext = createContext({
+  userName: "saher123",
+  name: "Sahir Zakaria",
+  email: "saher123@gmail.com",
+});
